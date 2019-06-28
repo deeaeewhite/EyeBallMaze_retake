454fa4f59ed39a2ef290f6952f6674895831091e
deeae <deeaeewhite@live.com>
2019-06-29 11:41:16 +1200

    Initial commit

---
+++ app/src/main/java/nz/ac/arastudent/eyeballmazeassignment2/model/IGame.java
@@ -0,0 +1,25 @@
+package nz.ac.arastudent.eyeballmazeassignment2.model;
+
+public interface IGame {
+	Integer getRowCount();
+	Integer getcolumnCount();
+	void restartMaze();
+	String makeMove(String move, int space);
+	Object[] whatsAt(int x, int y);
+	void updateMove();
+	void updateMaze();
+	void showNextMove();
+	//void start();
+	void clearConsole();
+	boolean isComplete();
+	String getItem(int x, int y);
+	Integer[] getPlayerLocation();
+    String getGoalCount();
+	void setGoalCount(String goalCount);
+    String getMoveCount();
+	void setMoveCount(String moveCount);
+	void setMovesLeft(String moveCount);
+    void setMazeCharacter(int x, int y, String item);
+	String[][] getGameMap();
+	Integer getMovesLeft();
+}
